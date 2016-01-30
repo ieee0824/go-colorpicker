ad6baacb726dc7b808b28a69113f0bc72a8eea82
shirase_aoi <ieee0824@ezweb.ne.jp>
2016-01-31 00:32:58 +0900

    ExtractTypicalColors function

---
+++ colorpicker.go
@@ -0,0 +1,66 @@
+package colorpicker
+
+import (
+	"fmt"
+	"image"
+
+	"github.com/lucasb-eyer/go-colorful"
+	"github.com/mdesenfants/gokmeans"
+)
+
+func min() {
+
+}
+
+func ExtractTypicalColors(img image.Image, k int) []colorful.Color {
+	indexs := make([]int, k)
+	w := img.Bounds().Max.X
+	h := img.Bounds().Max.Y
+	selectColor := make([]colorful.Color, 0, w*h)
+	for x := 0; x < w; x++ {
+		for y := 0; y < h; y++ {
+			r, g, b, _ := img.At(x, y).RGBA()
+			c := colorful.Color{
+				R: float64(r>>8) / 255,
+				G: float64(g>>8) / 255,
+				B: float64(b>>8) / 255,
+			}
+			_, s, v := c.Hsv()
+
+			if s > 0.5 && v > 0.5 {
+				selectColor = append(selectColor, c)
+			}
+		}
+	}
+
+	observations := func() []gokmeans.Node {
+		ret := make([]gokmeans.Node, len(selectColor))
+		for i := 0; i < len(selectColor); i++ {
+			ret[i] = func() []float64 {
+				h, s, v := selectColor[i].Hsv()
+				return []float64{h, s, v}
+			}()
+		}
+		return ret
+	}()
+	success, centroids := gokmeans.Train(observations, k, len(selectColor))
+	if success {
+		for _, observation := range observations {
+			index := gokmeans.Nearest(observation, centroids)
+			indexs[index]++
+		}
+	}
+	fmt.Println(indexs)
+
+	ret := make([]colorful.Color, 0, len(centroids))
+	for i, centroid := range centroids {
+		fmt.Println(float64(indexs[i]) / float64(len(observations)))
+		if float64(indexs[i])/float64(len(observations)) < 0.01 {
+			continue
+		}
+		hsv := centroid
+		ret = append(ret, colorful.Hsv(hsv[0], hsv[1], hsv[2]))
+	}
+
+	return ret
+}
